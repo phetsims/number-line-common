57d23d354801d6baf54051a57b82c41ebfe7f5d2
jbphet <john.blanco@colorado.edu>
2019-10-09 13:44:06 -0600

    changed "scenes" directory name to "explore", see #33

---
+++ js/explore/view/PiggyBankNode.js
@@ -0,0 +1,79 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * a node that represents a piggy bank
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ * @author Saurabh Totey
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const Image = require( 'SCENERY/nodes/Image' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const numberLineIntegers = require( 'NUMBER_LINE_INTEGERS/numberLineIntegers' );
+  const Path = require( 'SCENERY/nodes/Path' );
+  const piggyBankShapes = require( 'NUMBER_LINE_INTEGERS/scenes/view/piggyBankShapes' );
+  const Vector2 = require( 'DOT/Vector2' );
+
+  // images
+  const piggyBankWithFlowers = require( 'image!NUMBER_LINE_INTEGERS/piggy-bank-with-flowers.png' );
+  const piggyBankWithLightning = require( 'image!NUMBER_LINE_INTEGERS/piggy-bank-with-lightning.png' );
+
+  class PiggyBankNode extends Node {
+
+    /**
+     * @param {Object} [options]
+     */
+    constructor( options ) {
+
+      options = _.extend( {
+        fill: 'rgba( 0, 0, 0, 0 )', // initially transparent so that it is invisible but has size
+        lineWidth: 0,
+        decorationType: 'flowers'  // valid values are 'flowers' and 'lightning'
+      }, options );
+
+      const piggyBankOutlineNode = new Path( piggyBankShapes.MEDIUM_PIGGY_BANK_SHAPE, {
+        fill: options.fill,
+        lineWidth: options.lineWidth,
+        center: Vector2.ZERO
+      } );
+      const overlayImage = new Image(
+        options.decorationType === 'flowers' ? piggyBankWithFlowers : piggyBankWithLightning,
+        { opacity: 0.8 }
+      );
+      overlayImage.setScaleMagnitude( piggyBankOutlineNode.width / overlayImage.width );
+      overlayImage.center = Vector2.ZERO;
+      options.children = [ piggyBankOutlineNode, overlayImage ];
+      super( options );
+
+      // @private
+      this.outline = piggyBankOutlineNode;
+    }
+
+    /**
+     * @public
+     * @returns {ColorDef} the color of this piggy bank's fill
+     */
+    getFill() {
+      return this.outline.fill;
+    }
+
+    get fill() { return this.getFill(); }
+
+    /**
+     * @public
+     * @param {ColorDef} fill
+     */
+    setFill( fill ) {
+      this.outline.fill = fill;
+    }
+
+    set fill( fill ) { this.setFill( fill ); }
+
+  }
+
+  return numberLineIntegers.register( 'PiggyBankNode', PiggyBankNode );
+
+} );
