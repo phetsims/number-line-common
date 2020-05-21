61aa827356d0e07ad5c0a631ed32d4f44206c75d
jbphet <john.blanco@colorado.edu>
2020-05-21 13:25:47 -0600

    removed operation descriptions from the generic screen

--- js/common/view/SpatializedNumberLineNode.js
+++ js/common/view/SpatializedNumberLineNode.js
@@ -1,11 +1,5 @@
 // Copyright 2020, University of Colorado Boulder
 
-/**
- * SpatializedNumberLineNode is a Scenery node that presents a number line that is mapped into 2D space.
- *
- * @author John Blanco (PhET Interactive Simulations)
- */
-
 import Property from '../../../../axon/js/Property.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Orientation from '../../../../phet-core/js/Orientation.js';
@@ -36,6 +30,11 @@ const getIndicatorDistanceFromNL = ( numberLine, count ) => {
          ABSOLUTE_VALUE_SPAN_NL_DISTANCE_X + count * ABSOLUTE_VALUE_SPAN_SPACING_X;
 };
 
+/**
+ * SpatializedNumberLineNode is a Scenery Node that presents a number line that is mapped into 2D space.
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
 class SpatializedNumberLineNode extends Node {
 
   /**
