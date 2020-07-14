f5ff3baef752494c82dc3b210c9699d490f725b8
Saurabh Totey <SaurabhTotey@gmail.com>
2020-07-14 11:54:24 -0600

    Small documentation addition

--- js/common/model/PointsOffScaleCondition.js
+++ js/common/model/PointsOffScaleCondition.js
@@ -2,6 +2,7 @@
 
 /**
  * Represents when a SpatializedNumberLineNode shows its points off scale indicator
+ * SOME and ALL refer to how many points need to be off the scale on that side for the indicator to show up
  * NEVER means the indicator is never shown
  * SOME means the indicator is shown when at least one point is off on a side
  * ALL means that the indicator is only shown when all points are off on the same side
