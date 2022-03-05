058b9b30c5ae6a32fc11796366fd120f850f81cc
phet-dev <phetadmin@gmail.com>
2022-03-05 03:26:31 -0700

    update copyright dates from daily grunt work

--- js/common/view/AbsoluteValueSpanNode.js
+++ js/common/view/AbsoluteValueSpanNode.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * a Scenery node that represents the span of an absolute value separated from the number line
