7c042ca13deb732acc0dd28be42c643793f4502f
Marla Schulz <marlaa.schulz@gmail.com>
2023-11-17 15:39:21 -0600

    Remove icons from RegionAndCultureComboBox.ts, see: https://github.com/phetsims/number-line-integers/issues/118

--- js/explore/view/NumberLineCharacterSet.js
+++ js/explore/view/NumberLineCharacterSet.js
@@ -15,14 +15,13 @@ export default class NumberLineCharacterSet extends RegionAndCulturePortrayal {
 
   /**
    *
-   * @param { Image } headshotIcon
    * @param { LocalizedStringProperty } label
    * @param { HTMLImageElement } screenHomeIcon
    * @param { HTMLImageElement } screenNavIcon
    * @param { string } queryParameterValue
    */
-  constructor( headshotIcon, label, screenHomeIcon, screenNavIcon, queryParameterValue ) {
-   super( headshotIcon, label, queryParameterValue, {} );
+  constructor( label, screenHomeIcon, screenNavIcon, queryParameterValue ) {
+   super( label, queryParameterValue, {} );
 
     this.screenHomeIcon = screenHomeIcon;
     this.screenNavIcon = screenNavIcon;
