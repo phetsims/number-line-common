825c02eae3e0b235fd9774c177e411d8a828a664
phet-dev <phetadmin@gmail.com>
2024-03-12 03:20:05 -0600

    update copyright dates from daily grunt work

--- js/explore/view/NumberLinePortrayal.js
+++ js/explore/view/NumberLinePortrayal.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * NumberLinePortrayal defines the common properties for each portrayal in the Number Line Suite.
